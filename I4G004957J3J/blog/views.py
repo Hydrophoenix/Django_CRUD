607c2561ecb3bd646da341879fc1bb5bc745e3ac
Hydrophoenix <nkwochaugo2006@gmail.com>
2022-06-22 21:23:13 +0100

    Task submission

---
+++ I4G004957J3J/blog/views.py
@@ -0,0 +1,28 @@
+from msilib.schema import ListView
+from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from I4G004957J3J.blog.models import Post
+
+# Create your views here.
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
